a6e4b7f866059043274b2184f5506dbd35762c0c
XavierPerras <89546660+XavierPerras@users.noreply.github.com>
2024-10-19 12:33:06 -0400

    fix img projets and scrolly header, footer

--- theme_Porfolio/footer.php
+++ theme_Porfolio/footer.php
@@ -1,42 +1,42 @@
-
 <section class="footer ">
-        <div class="wrapper">
-            <div class="head-footer">
-                <a data-scrolly='fromBottom' href="https://xperras.dectim.ca" class="header__brand">
-                <h2>X<span>avier</span><span>&nbsp;</span>P<span>erras</span></h2>
+    <div class="wrapper">
+        <div class="head-footer">
+            <a href="https://xperras.dectim.ca" class="header__brand">
+                <h2 data-scrolly='fromLeft'>X<span>avier</span><span>&nbsp;</span>P<span>erras</span></h2>
+            </a>
+            <div data-scrolly='fromRight' class="media nav-primary nav-footer">
+                <a href="https://github.com/XavierPerras">
+                    <svg class="icon icon--lg">
+                        <use xlink:href="#icon-githubLogo"></use>
+                    </svg>
                 </a>
-                <div class="media nav-primary nav-footer">
-                    <a href="https://github.com/XavierPerras">
-                        <svg class="icon icon--lg">
-                            <use xlink:href="#icon-githubLogo"></use>
-                        </svg>
-                    </a>
-                    <a href="https://sketchfab.com/WonderedGnat082">
-                        <svg class="icon icon--lg">
-                            <use xlink:href="#icon-sketchfabLogo"></use>
-                        </svg>
-                    </a>
-                    <a href="https://www.instagram.com/xavier_perras/">
-                        <svg class="icon icon--lg">
-                            <use xlink:href="#icon-instaLogo"></use>
-                        </svg>
-                    </a>
-                    <a href="https://www.linkedin.com/in/xavier-perras-3746b932a/">
-                        <svg class="icon icon--lg">
-                            <use xlink:href="#icon-linkinLogo"></use>
-                        </svg>
-                    </a>
-                    <a href="https://www.youtube.com/@xavierperras">
-                        <svg class="icon icon--lg">
-                            <use xlink:href="#icon-ytLogo"></use>
-                        </svg>
-                    </a>
-                </div>
-
+                <a href="https://sketchfab.com/WonderedGnat082">
+                    <svg class="icon icon--lg">
+                        <use xlink:href="#icon-sketchfabLogo"></use>
+                    </svg>
+                </a>
+                <a href="https://www.instagram.com/xavier_perras/">
+                    <svg class="icon icon--lg">
+                        <use xlink:href="#icon-instaLogo"></use>
+                    </svg>
+                </a>
+                <a href="https://www.linkedin.com/in/xavier-perras-3746b932a/">
+                    <svg class="icon icon--lg">
+                        <use xlink:href="#icon-linkinLogo"></use>
+                    </svg>
+                </a>
+                <a href="https://www.youtube.com/@xavierperras">
+                    <svg class="icon icon--lg">
+                        <use xlink:href="#icon-ytLogo"></use>
+                    </svg>
+                </a>
             </div>
-            <p data-scrolly='fromBottom'>© Xavier Perras. Tous droits réservés.</p>
+
         </div>
-    </section>
+        <p data-scrolly='fromBottom'>© Xavier Perras. Tous droits réservés.</p>
+    </div>
+</section>
 <?php wp_footer(); ?>
-    </body>
+</body>
+
 </html>
--- theme_Porfolio/header.php
+++ theme_Porfolio/header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+    <title> <?php wp_title('|', true, 'right');
+            bloginfo('name'); ?></title>
     <?php wp_head(); ?>
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/dist/styles/main.css">
     <script>
@@ -12,24 +14,25 @@
     </script>
     <script src="<?php bloginfo('template_url'); ?>/dist/scripts/main.js" defer></script>
 </head>
-<body <?php body_class(); ?> data-component="Scrolly" > 
+
+<body <?php body_class(); ?> data-component="Scrolly">
 
 
-<header class="header" data-component="Header">
+    <header class="header" data-component="Header">
         <div class="wrapper">
             <div class="head">
-                <a data-scrolly='fromLeft' href="https://xperras.dectim.ca" class="header__brand">
-                <h2>X<span>avier</span><span>&nbsp;</span>P<span>erras</span></h2>
+                <a href="https://xperras.dectim.ca" class="header__brand">
+                    <h2 data-scrolly='fromLeft'>X<span>avier</span><span>&nbsp;</span>P<span>erras</span></h2>
                 </a>
 
-                
+
 
                 <nav data-scrolly='fromRight' class="nav-primary"><?php wp_nav_menu(array(
-                'theme_location' => 'menu_principal',
-                'container' => 'ul',//create a balise nav
-               
-            )); ?>
-                   
+                                                                        'theme_location' => 'menu_principal',
+                                                                        'container' => 'ul', //create a balise nav
+
+                                                                    )); ?>
+
                 </nav>
                 <button data-scrolly="fromRight" class="header__toggle js-toggle">
                     <span></span>
