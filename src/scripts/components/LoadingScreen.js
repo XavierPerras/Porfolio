867a4b02af713d973b2005207b2f98756b1ace32
XavierPerras <89546660+XavierPerras@users.noreply.github.com>
2024-10-28 18:25:24 -0400

    updrade the loader with outro

--- src/scripts/components/LoadingScreen.js
+++ src/scripts/components/LoadingScreen.js
@@ -1,35 +1,33 @@
 export default class LoadingScreen {
   constructor() {
     this.init();
-    this.LoadingScreen();
   }
 
   init() {
+    const loadingScreen = document.getElementById('loading-screen');
+    const content = document.getElementById('content');
     let loadingComplete = false;
-    const minLoadingTime = 3000; // Minimum loading time in milliseconds (3 seconds)
+    const minLoadingTime = 3000;
     let loadingTimer;
 
-    // Wait for all resources to load
-    window.addEventListener('load', function () {
-      loadingComplete = true; // Mark loading as complete
-      clearTimeout(loadingTimer); // Clear the timer if the page loads early
-      setTimeout(hideLoadingScreen, minLoadingTime); // Ensure we hide the preloader after minimum time
+    window.addEventListener('load', () => {
+      loadingComplete = true;
+      clearTimeout(loadingTimer);
+      setTimeout(hideLoadingScreen, minLoadingTime);
     });
 
-    // Start the loading timer for minimum display duration
     loadingTimer = setTimeout(() => {
-      if (!loadingComplete) {
-        hideLoadingScreen(); // Hide if still showing after timeout
-      }
-    }, minLoadingTime); // Show for at least 3 seconds
+      if (!loadingComplete) hideLoadingScreen();
+    }, minLoadingTime);
 
     function hideLoadingScreen() {
-      const loadingScreen = document.getElementById('loading-screen');
-      const content = document.getElementById('content');
-
-      loadingScreen.style.display = 'none'; // Hide loading screen
-      content.style.display = 'block'; // Show main content
+      loadingScreen.classList.add('outro'); // Trigger outro animation
+      setTimeout(() => {
+        loadingScreen.style.display = 'none';
+        content.style.display = 'block';
+      }, 800); // Match outro animation duration
     }
   }
 }
+
 new LoadingScreen();
